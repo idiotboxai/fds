032465476764ab3fb942b4061c62ce22c71d1d91
Anirudh patki <83394182+idiotboxai@users.noreply.github.com>
2023-11-30 12:04:00 +0530

    Update GroupA_Practical2(python).py

--- GroupA_Practical2(python).py
+++ GroupA_Practical2(python).py
@@ -1,1 +1,8 @@
+#GroupA_Practical2
+'''Write a Python program to store marks scored in subject “Fundamental of Data
+Structure” by N students in the class. Write functions to compute following:
+a) The average score of class
+b) Highest score and lowest score of class
+c) Count of students who were absent for the test
+d) Display mark with highest frequency'''
 
