c6e18ec0c132b718fc22e05cfbb08f14e3f4cbfa
Anirudh patki <83394182+idiotboxai@users.noreply.github.com>
2023-12-12 18:38:46 +0530

    Update GroupD_Practical2(cpp).cpp

--- Group-D/GroupD_Practical2(cpp).cpp
+++ Group-D/GroupD_Practical2(cpp).cpp
@@ -1,130 +1,96 @@
 /*
 
-Implement C++ program for expression conversion as infix to postfix and its evaluation
-using stack based on given conditions:
-1. Operands and operator, both must be single character.
-2. Input Postfix expression must be in a desired format.
-3. Only '+', '-', '*' and '/ ' operators are expected.
-
- */
+In any language program mostly syntax error occurs due to unbalancing delimiter such as
+(),{},[]. Write C++ program using stack to check whether given expression is well parenthesized or not.
+*/
 
 
-#include<iostream>
-#include<conio.h>
+#include <iostream>
 using namespace std;
-class stack
+#define size 10
+
+class stackexp
 {
- public:
-  char stack_array[50];
-  int top;
-  stack()
-  {
-    top=-1;
-  }
-void push(char symbol)
-{  if(full())
-      cout<<"\nStack overflow:\n";
-    else
-    { top=top+1;
-      stack_array[top]=symbol;
-     }
-}
-char pop()
-{   if(empty())
-       return('#');         // Return value '#' indicates stack is empty
-     else
-       return(stack_array[top--]);
-}
-int empty()
-{    if(top==-1)
-       return(1);
-     else
-       return(0);
-}
-int full()
-{    if(top==49)
-       return(1);
-     else
-       return(0);
-}
-private:
-  char infix[50];
-  char postfix[50];
- public:
-    void read()
+    int top;
+    char stk[size];
+public:
+    stackexp()
     {
-      cout<<"\nEnter an infix expression:";
-      cin>>infix;
+     top=-1;
     }
-int white_space(char symbol)
-  {  if(symbol==' ' || symbol=='\t' || symbol=='\0')
-         return 1;
-      else
-        return 0;
-  }
-void ConvertToPostfix()
- {    int prev,p;
-      char entry;
-      p=0;
-      for(int i=0;infix[i]!='\0';i++)
-      {
-    if(!white_space(infix[i]))
-    { switch(infix[i])
-      {
-        case '(': push(infix[i]);
-                  break;
-        case ')': while((entry=pop())!='(')
-                  postfix[p++]=entry;
-                  break;
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-        if(!empty())
-          {  prev=prior(infix[i]);
-             entry=pop();
-         while(prev<=prior(entry))
-         {  postfix[p++]=entry;
-           if(!empty())
-              entry=pop();
-           else
-              break;
-         }
-        if(prev>prior(entry))
-           push(entry);
-        }
-          push(infix[i]);
-          break;
-          default:
-          postfix[p++]=infix[i];
-          break;
-        }
-      }
-    }
-      while(!empty())                //while stack is not empty
-       postfix[p++]=pop();
-    postfix[p]='\0';
-    cout<<"\nThe postfix expression is: "<<postfix<<endl;
-  }
-int prior(char symbol)
-{  switch(symbol)
-    { case '/': return(4);          // Precedence of / is 4
-      case '*': return(3);          // Precedence of * is 3
-      case '+': return(2);          // Precedence of + is 2
-      case '-': return(1);          // Precedence of - is 1
-      case '(': return(0);          // Precedence of ( is 0
-      default: return(-1);
-    }
-  }
+    void push(char);
+    char pop();
+    int isfull();
+    int isempty();
 };
+
+void stackexp::push(char x)
+{
+    top=top+1;
+    stk[top]=x;
+}
+
+char stackexp::pop()
+{
+    char s;
+    s=stk[top];
+    top=top-1;
+    return s;
+}
+
+int stackexp::isfull()
+{
+    if(top==size)
+        return 1;
+    else
+        return 0;
+}
+
+int stackexp::isempty()
+{
+    if(top==-1)
+        return 1;
+    else
+        return 0;
+}
+
 int main()
-{  char choice='y';
-   stack expr;
-   while(choice=='y')
-  {expr.read();
-   expr.ConvertToPostfix();
-   cout<<"\n\nDo you want to continue ? (y/n): ";
-   cin>>choice;
- }
- return 0;
+{
+    stackexp s1;
+    char exp[20],ch;
+    int i=0;
+    cout << "\n\t!! Parenthesis Checker..!!!!" << endl; // prints !!!Hello World!!!
+    cout<<"\nEnter the expression to check whether it is in well form or not :  ";
+    cin>>exp;
+    if((exp[0]==')')||(exp[0]==']')||(exp[0]=='}'))
+    {
+        cout<<"\n Invalid Expression.....\n";
+        return 0;
+    }
+    else
+    {
+        while(exp[i]!='\0')
+        {
+            ch=exp[i];
+            switch(ch)
+            {
+            case '(':s1.push(ch);break;
+            case '[':s1.push(ch);break;
+            case '{':s1.push(ch);break;
+            case ')':s1.pop();break;
+            case ']':s1.pop();break;
+            case '}':s1.pop();break;
+            }
+            i=i+1;
+        }
+    }
+    if(s1.isempty())
+    {
+        cout<<"\nExpression is well parenthesised...\n";
+    }
+    else
+    {
+        cout<<"\nSorry !!! Invalid Expression or not in well parenthesized....\n";
+    }
+    return 0;
 }
